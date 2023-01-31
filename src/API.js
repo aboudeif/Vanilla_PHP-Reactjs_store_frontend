e7fb7e01f1293688f026383d62357ef15aa3414d
Abdallah Aboudeif <71098171+aboudeif@users.noreply.github.com>
2023-01-31 22:35:16 +0200

    fix bugs

--- src/API.js
+++ src/API.js
@@ -8,7 +8,6 @@ if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
   Accept: "application/json",
-  Authorization: token,
 };
 
 export const add = (product) =>
